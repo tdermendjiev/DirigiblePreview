e265d8c87010d84d824836f499993d7c1079ae25
Dermendzhiev, Teodor (external - Project) <tdermendjievft@gmail.com>
2022-07-18 17:54:08 +0300

    Initial Commit

---
+++ DirigiblePreview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DirigiblePreview
+//
+//  Created by Dermendzhiev, Teodor (external - Project) on 18.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
